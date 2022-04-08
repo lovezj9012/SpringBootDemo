8af8ff6533953d1a2ca1bfbf2021c111558a3247
lovezj9012 <843265303@qq.com>
2022-04-08 17:15:05 +0800

    修改获取不到属性值

--- src/main/java/com/zj/domainmodel/Dog.java
+++ src/main/java/com/zj/domainmodel/Dog.java
@@ -1,11 +1,29 @@
 package com.zj.domainmodel;
 
-import lombok.Data;
-
-@Data
 public class Dog {
 	
+	@Override
+	public String toString() {
+		return "Dog [name=" + name + ", age=" + age + "]";
+	}
+
 	private String name;
     
-    private Integer age;
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	private Integer age;
 }
--- src/main/java/com/zj/domainmodel/Person.java
+++ src/main/java/com/zj/domainmodel/Person.java
@@ -6,16 +6,16 @@ import java.util.Map;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import lombok.Data;
-
 /**
  * 将配置文件中配置的每个属性值，银蛇到这个组件中
  * @ConfigurationProperties 告诉SpringBoot将本类中的所有属性和配置文件中的属性进行绑定 
  * prefix = "person" 配置文件中的哪个下面的所有属性进行一一映射
  * 
  * @Component只有这个组件是容器中的组件，才能使用容器提供的@ConfigurationProperties
+ * 
+ * 单元测试想要获取属性值，不能使用@Data注解
  */
-@Data
+
 @Component
 @ConfigurationProperties(prefix = "person")
 public class Person {
@@ -27,6 +27,46 @@ public class Person {
 	private Map<String, Object> maps;
 
 	private List<Object> lists;
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Map<String, Object> getMaps() {
+		return maps;
+	}
+
+	public void setMaps(Map<String, Object> maps) {
+		this.maps = maps;
+	}
+
+	public List<Object> getLists() {
+		return lists;
+	}
+
+	public void setLists(List<Object> lists) {
+		this.lists = lists;
+	}
+
+	public Dog getDog() {
+		return dog;
+	}
+
+	public void setDog(Dog dog) {
+		this.dog = dog;
+	}
 
 	private Dog dog;
 
